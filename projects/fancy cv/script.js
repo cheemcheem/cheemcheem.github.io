3295f8e6540c64c66a4bcd34ab7a6e63d8db6567
Kathan Cheema <kc67@st-andrews.ac.uk>
2018-06-01 04:13:00 +0100

    Started work on Promises.

--- projects/fancy cv/script.js
+++ projects/fancy cv/script.js
@@ -1,4 +1,4 @@
-const test = "Lorem ipsum dolor sit amet, te nec altera quaestio. Te tale quaestio vis, duo\n                    verterem periculis ad. Facer eripuit ut pri, ex hinc posse quaerendum cum. Eam in sonet dicant\n                    salutandi, simul intellegam vim in. Audire scriptorem ex his, sit veniam semper eripuit at, ne pri\n                    cetero docendi aliquando. No aliquip fuisset duo, no modo affert scripserit est.\n\n                    Esse dolor adipiscing vix et, harum tamquam nec eu, an qui sint atomorum. Id vis nonumy causae\n                    graecis, evertitur consetetur consequuntur an usu. Inermis ceteros sadipscing vix ad, ad assum\n                    expetenda ius. Veniam postea eos at, cibo saepe ceteros per no. Ut erant vivendo eum, cu has autem\n                    diceret postulant. Ferri dicta ludus pri ex, ceteros prodesset quo ne, sea tractatos adipiscing te.\n\n                    Suas legimus adolescens has at, ne soluta inciderint sit, usu ne odio latine vulputate. His stet\n                    nostrud lobortis ad. Ne pro veri oporteat deseruisse, his aliquam perfecto honestatis cu, vix ne\n                    nostrum lobortis consetetur. An noster scripta senserit mei, est ullum sensibus dissentiunt an,\n                    definiebas definitiones no pri. Ut virtute expetendis has, est ea soluta perpetua rationibus.\n\n                    Augue dolores definitiones ut quo. Vix eripuit delectus appetere te, pri at aeterno nostrud\n                    intellegebat. Vix prima timeam interpretaris an, nec dolorum accusata ea, et vix quas partiendo\n                    contentiones. Admodum forensibus quaerendum vis in.\n\n                    Pri facilis insolens reprimique ex, cu his graeco latine. In eam utinam invidunt inimicus, te\n                    oblique corpora eos, vis ad viris partem percipitur. Decore utinam eos id. Vidit malis suscipiantur\n                    te mea, et cum pericula principes.\n\n                    Vis ut veri novum errem, ex inani aeterno omnesque his. Nam semper dolorum maluisset ne, an aeterno\n                    feugiat omittam eam. Et sit doctus minimum, duo eu congue iriure dissentiunt, nam diam dicta\n                    menandri ei. Cu nec soleat ullamcorper, mei nostro deseruisse no. Pri legere vocibus in, ad mea odio\n                    primis maiestatis.\n\n                    No aperiam officiis iudicabit vel, id qui recusabo convenire signiferumque. Eum movet aperiri ea,\n                    pro no quis choro suavitate. Mel intellegat mnesarchum definitionem in, sed nostrud nominavi\n                    expetendis in, ne errem omnesque nec. An has eros eruditi salutandi, melius dolorum et vim.\n\n                    Eum id porro definitionem, malis ubique tritani est ei. Vix modo animal dolorem ut, duo tota feugiat\n                    no. Prompta recusabo sit ei, quo partiendo mediocritatem ea, ei autem erant qualisque mei. Oblique\n                    bonorum gubergren his ne. Est ad dicta persecuti, ea eum graecis fuisset.\n\n                    Ne per odio ponderum. Possit quodsi contentiones mei ad, et ius diceret bonorum. Vix ne cibo\n                    hendrerit, cum at alia facer. Vide decore ne sit, sumo dolorem principes mea ne. Et vidit vivendum\n                    pro, audire signiferumque sea ut.\n\n                    Ex vel fuisset volutpat eloquentiam, pri ceteros suscipit suavitate ea. At esse debitis efficiantur\n                    sed, ei offendit assentior mea, his cu viderer ceteros. Ut nisl partiendo pertinacia cum, ut sit\n                    veniam sadipscing. Ei summo temporibus est, est in doctus offendit convenire. Ei labore molestie\n                    pri.";
+//const test = "Lorem ipsum dolor sit amet, te nec altera quaestio. Te tale quaestio vis, duo\n                    verterem periculis ad. Facer eripuit ut pri, ex hinc posse quaerendum cum. Eam in sonet dicant\n                    salutandi, simul intellegam vim in. Audire scriptorem ex his, sit veniam semper eripuit at, ne pri\n                    cetero docendi aliquando. No aliquip fuisset duo, no modo affert scripserit est.\n\n                    Esse dolor adipiscing vix et, harum tamquam nec eu, an qui sint atomorum. Id vis nonumy causae\n                    graecis, evertitur consetetur consequuntur an usu. Inermis ceteros sadipscing vix ad, ad assum\n                    expetenda ius. Veniam postea eos at, cibo saepe ceteros per no. Ut erant vivendo eum, cu has autem\n                    diceret postulant. Ferri dicta ludus pri ex, ceteros prodesset quo ne, sea tractatos adipiscing te.\n\n                    Suas legimus adolescens has at, ne soluta inciderint sit, usu ne odio latine vulputate. His stet\n                    nostrud lobortis ad. Ne pro veri oporteat deseruisse, his aliquam perfecto honestatis cu, vix ne\n                    nostrum lobortis consetetur. An noster scripta senserit mei, est ullum sensibus dissentiunt an,\n                    definiebas definitiones no pri. Ut virtute expetendis has, est ea soluta perpetua rationibus.\n\n                    Augue dolores definitiones ut quo. Vix eripuit delectus appetere te, pri at aeterno nostrud\n                    intellegebat. Vix prima timeam interpretaris an, nec dolorum accusata ea, et vix quas partiendo\n                    contentiones. Admodum forensibus quaerendum vis in.\n\n                    Pri facilis insolens reprimique ex, cu his graeco latine. In eam utinam invidunt inimicus, te\n                    oblique corpora eos, vis ad viris partem percipitur. Decore utinam eos id. Vidit malis suscipiantur\n                    te mea, et cum pericula principes.\n\n                    Vis ut veri novum errem, ex inani aeterno omnesque his. Nam semper dolorum maluisset ne, an aeterno\n                    feugiat omittam eam. Et sit doctus minimum, duo eu congue iriure dissentiunt, nam diam dicta\n                    menandri ei. Cu nec soleat ullamcorper, mei nostro deseruisse no. Pri legere vocibus in, ad mea odio\n                    primis maiestatis.\n\n                    No aperiam officiis iudicabit vel, id qui recusabo convenire signiferumque. Eum movet aperiri ea,\n                    pro no quis choro suavitate. Mel intellegat mnesarchum definitionem in, sed nostrud nominavi\n                    expetendis in, ne errem omnesque nec. An has eros eruditi salutandi, melius dolorum et vim.\n\n                    Eum id porro definitionem, malis ubique tritani est ei. Vix modo animal dolorem ut, duo tota feugiat\n                    no. Prompta recusabo sit ei, quo partiendo mediocritatem ea, ei autem erant qualisque mei. Oblique\n                    bonorum gubergren his ne. Est ad dicta persecuti, ea eum graecis fuisset.\n\n                    Ne per odio ponderum. Possit quodsi contentiones mei ad, et ius diceret bonorum. Vix ne cibo\n                    hendrerit, cum at alia facer. Vide decore ne sit, sumo dolorem principes mea ne. Et vidit vivendum\n                    pro, audire signiferumque sea ut.\n\n                    Ex vel fuisset volutpat eloquentiam, pri ceteros suscipit suavitate ea. At esse debitis efficiantur\n                    sed, ei offendit assentior mea, his cu viderer ceteros. Ut nisl partiendo pertinacia cum, ut sit\n                    veniam sadipscing. Ei summo temporibus est, est in doctus offendit convenire. Ei labore molestie\n                    pri.";
 $(document).ready(function () {
     checkDataOffset();
     resetNavOnSizeChange();
@@ -10,17 +10,17 @@ $(document).ready(function () {
 
     if (window.innerWidth < 768) {
         // addProjects(function () {
-            addEducation(function () {
-                addLanguages(function () {
-                    console.log("Finished.")
-                })
+        addEducation(function () {
+            addLanguages(function () {
+                console.log("Finished.")
             })
+        })
         // })
     } else {
         addLanguages(function () {
             addEducation(function () {
                 // addProjects(function () {
-                    console.log("Finished.")
+                console.log("Finished.")
                 // })
             })
         })
@@ -92,7 +92,7 @@ function resetNavOnSizeChange() {
     navItemsImage.removeClass();
     navItemsUl.removeClass();
 
-    if($(window).width() < 768) {
+    if ($(window).width() < 768) {
         navDiv.addClass(topNavDivClasses);
         nav.addClass(topNavClasses);
         navDiv.css('position', 'relative');
@@ -113,6 +113,7 @@ function resetNavOnSizeChange() {
         navItemsUl.css('width:100%');
     }
 }
+
 function checkDataOffset() {
     if (window.innerWidth < 768) {
         $('<body>').attr('data-offset', "62");
@@ -122,42 +123,97 @@ function checkDataOffset() {
     }
 }
 
-function addEducation(callback) {
-    $(".education").addClass("visible");
-    $("#Education")
-        .css("display", "block")
-        .css("visibility", "visible")
-        .hide()
-        .fadeIn(
-            500,
-            "swing",
-            function () {
-                if (window.innerWidth < 768) {
-                    displayUni(
-                        function () {
-                            return displayIT(
-                                function () {
-                                    return displaySchool(callback)
-                                }
-                            )
-                        }
-                    )
-                } else {
-                    displaySchool(
-                        function () {
-                            return displayIT(
-                                function () {
-                                    return displayUni(callback)
-                                }
-                            )
-                        }
-                    )
-                }
 
-            }
-        ).after("<br>")
-    ;
+function addEducationP() {
+    return new Promise((resolve, reject) => {
+        let p1 = new Promise(((resolve1, reject1) => {
+            $(".education").addClass("visible")
+                .done(() => resolve1())
+                .fail(() => reject1());
+        }));
+        let p2 = new Promise(((resolve2, reject2) => {
+            $("#Education")
+                .css("display", "block")
+                .css("visibility", "visible")
+                .hide()
+                .fadeIn(
+                    500,
+                    "swing",
+                    function () {
+                        if (window.innerWidth < 768) {
+                            displayUni(
+                                function () {
+                                    return displayIT(
+                                        function () {
+                                            return displaySchool(callback)
+                                        }
+                                    )
+                                }
+                            )
+                        } else {
+                            displaySchool(
+                                function () {
+                                    return displayIT(
+                                        function () {
+                                            return displayUni(callback)
+                                        }
+                                    )
+                                }
+                            )
+                        }
+
+                    }
+                )
+                .after("<br>")
+                .done(() => resolve2())
+                .fail(() => reject2());
+        }));
+
+        p1.then(() => p2).then(() => resolve()).catch((err) => reject(err))
+
+    });
 }
+
+
+function addEducation(callback) {
+    // $(".education").addClass("visible");
+    // $("#Education")
+    //     .css("display", "block")
+    //     .css("visibility", "visible")
+    //     .hide()
+    //     .fadeIn(
+    //         500,
+    //         "swing",
+    //         function () {
+    //             if (window.innerWidth < 768) {
+    //                 displayUni(
+    //                     function () {
+    //                         return displayIT(
+    //                             function () {
+    //                                 return displaySchool(callback)
+    //                             }
+    //                         )
+    //                     }
+    //                 )
+    //             } else {
+    //                 displaySchool(
+    //                     function () {
+    //                         return displayIT(
+    //                             function () {
+    //                                 return displayUni(callback)
+    //                             }
+    //                         )
+    //                     }
+    //                 )
+    //             }
+    //
+    //         }
+    //     ).after("<br>")
+    // ;
+
+    addEducationP().then(() => callback())
+}
+
 function displayUni(callback) {
     displayEducationComponent(
         "uni",
@@ -173,6 +229,7 @@ function displayUni(callback) {
         callback
     );
 }
+
 function displayIT(callback) {
     displayEducationComponent(
         "openIT",
@@ -184,6 +241,7 @@ function displayIT(callback) {
         true,
         callback);
 }
+
 function displaySchool(callback) {
     displayEducationComponent(
         "school",
@@ -199,15 +257,16 @@ function displaySchool(callback) {
         callback
     );
 }
+
 function displayEducationComponent(id, h4, h5, em, cards, lines, breakAfter, callback) {
     const preSelect = "#Education div.card-body div#" + id;
     if (breakAfter) {
         $(preSelect).append("<br>");
     }
     display($(preSelect + " h4"), h4, 10, 1,
-        function() {
+        function () {
             display($(preSelect + " h5"), h5, 10, 1,
-                function() {
+                function () {
                     display($(preSelect + " em"), em, 10, 1,
                         function () {
                             if (cards !== null && lines !== null) {
@@ -258,7 +317,6 @@ function displayEducationComponent(id, h4, h5, em, cards, lines, breakAfter, cal
                                 });
 
 
-
                             } else {
                                 setTimeout(callback, 100);
                             }
@@ -289,15 +347,15 @@ function addLanguages(callback) {
                     function addLogo(index, files, names) {
                         if (index < files.length) {
                             const div = $("<div>", {class: "col-2 col-lg", style: "visibility: hidden;"});
-                            const image = $("<img>", {src: "images/logos/" + files[index], class : "media-object logo"});
+                            const image = $("<img>", {src: "images/logos/" + files[index], class: "media-object logo"});
                             const title = $("<div>", {class: "col-4 d-lg-none align-middle"}).append(names[index]);
                             div.append(image).css("opacity", "0").css('visibility', 'visible');
                             image.addClass("padded-bottom");
                             title.addClass("padded-bottom").css("font-size", "medium").css("opacity", "0");
 
                             $("div#Languages.card div.card-body div.row").append(div).append(title);
-                            div.animate({ opacity: 1 }, "fast");
-                            title.animate({ opacity: 1 }, "fast");
+                            div.animate({opacity: 1}, 200);
+                            title.animate({opacity: 1}, 200);
 
                             setTimeout(function () {
                                 addLogo(index + 1, files, names);
@@ -307,6 +365,7 @@ function addLanguages(callback) {
 
                         }
                     }
+
                     const names = ["Java", "C", "JavaScript", "Swift", "Python", "Haskell", "AppleScript", "MySQL"];
                     const files = ["java.png", "c.png", "js.jpg", "swift.png", "python.png", "haskell.png", "applescript.png", "mysql.svg"];
                     addLogo(0, files, names);
@@ -328,6 +387,7 @@ function addProjects(callback) {
     ;
 
 }
+
 function newCard(header, body, id, headerType) {
     const $card = $("<div>", {class: "card"});
 
@@ -362,5 +422,6 @@ function display($parent, child, time, count, callback) {
             }, time);
         }
     }
+
     update(0);
 }
